4428acf16e7f00e855795a97ee5a5a2034de6c5f
Erkinai <erkinai.abdillakyzy.bj@gmail.com>
2023-05-02 22:52:30 +0600

    first commit

---
+++ lib/components/status_card_for_slider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sabak17_bmi_ulantuu/constants/app_colors.dart';
+
+class StatusCardSlider extends StatelessWidget {
+  const StatusCardSlider(
+      {super.key,
+      required this.text,
+      required this.text1,
+      required this.text2});
+
+  final String text, text1, text2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        color: AppColors.cardcolor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Text(
+                  text1,
+                  style: const TextStyle(
+                      fontSize: 40, fontWeight: FontWeight.w800),
+                ),
+                Text(
+                  text2,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w800),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 300,
+              child: CupertinoSlider(
+                  value: 180,
+                  min: 0,
+                  max: 240,
+                  activeColor: AppColors.whitecolor,
+                  thumbColor: const Color(0xffFF0F65),
+                  onChanged: (value) {}),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
